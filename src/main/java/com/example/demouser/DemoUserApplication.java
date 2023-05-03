23638c6b04b64ccf94e8e9d1e27d673209552199
tiendainguyen <daint.dev@gmail.com>
2023-05-03 23:47:14 +0700

    init

---
+++ src/main/java/com/example/demouser/DemoUserApplication.java
@@ -0,0 +1,25 @@
+package com.example.demouser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/gateway")
+@SpringBootApplication
+public class DemoUserApplication {
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUserApplication.class, args);
+	}
+	@KafkaListener(topics = "authen_topic", groupId = "authen_service")
+	public void receive(String user) {
+    System.out.println("user");
+		kafkaTemplate.send("authen_topic","this is dai from user service");
+	}
+}
---
+++ src/test/java/com/example/demouser/DemoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
